ec09e96fc292fd8e4b6c4989aa8bb8080a104a30
ethancui <holmeseason@gmail.com>
2018-07-19 14:41:57 +0800

    更新通用adapter

--- app/src/main/java/com/cui/mdc/mdcBasic/SingleTypeListAdapter.kt
+++ app/src/main/java/com/cui/mdc/mdcBasic/SingleTypeListAdapter.kt
@@ -1,8 +1,5 @@
 package com.cui.mdc.mdcBasic
 
-import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleObserver
-import android.arch.lifecycle.OnLifecycleEvent
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
@@ -18,7 +15,7 @@ import com.widget.library.refresh.recyclerview.DDRecyclerViewLayout
  * 这里只是控制在数据过于少和数据量恢复后停止和恢复上拉下拉加载
 */
  */
-abstract class SingleTypeListAdapter<T, B : ViewDataBinding>(ddRecyclerViewLayout: DDRecyclerViewLayout) : AbstractBaseAdapter<T>(ddRecyclerViewLayout), LifecycleObserver {
+abstract class SingleTypeListAdapter<T, B : ViewDataBinding>(ddRecyclerViewLayout: DDRecyclerViewLayout) : AbstractBaseAdapter<T>(ddRecyclerViewLayout) {
     lateinit var binding: B
 
     override fun onBindViewHolder(holder1: RecyclerView.ViewHolder, position: Int) {
@@ -38,9 +35,4 @@ abstract class SingleTypeListAdapter<T, B : ViewDataBinding>(ddRecyclerViewLayou
     }
 
     abstract fun onItemBinding(binding: B, item: T, position: Int)
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    private fun clearItemTypeMapAndData() {
-        super.data.clear()
-    }
 }
